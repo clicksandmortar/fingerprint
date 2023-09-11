b35e896323d705255dbaf3a2dd0d8bca222a2e86
Scott Pringle <pringlesc91@gmail.com>
2023-09-11 15:50:47 +0100

    Remove Sentry Replay as this currently breaks Bookings

--- src/context/FingerprintContext.tsx
+++ src/context/FingerprintContext.tsx
@@ -16,13 +16,9 @@ Sentry.init({
       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
       tracePropagationTargets: ['localhost:8000', 'https:yourserver.io/api/']
     }),
-    new Sentry.Replay()
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
 
 const queryClient = new QueryClient()
