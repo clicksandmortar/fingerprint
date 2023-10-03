921fa3e0d98864074855bd8f9baa01f7e568937a
Ed 'Catboy' Putans <edputans@gmail.com>
2023-10-03 11:44:05 +0300

    build

--- dist/client/handler.d.ts
+++ dist/client/handler.d.ts
@@ -5,6 +5,7 @@ export declare type Handler = {
     id?: string;
     invocation?: 'INVOCATION_UNSPECIFIED' | 'INVOCATION_IDLE_TIME' | 'INVOCATION_EXIT_INTENT' | 'INVOCATION_PAGE_LOAD';
     behaviour?: string;
+    delay?: number;
     invoke?: TriggerCallback;
 };
 export declare const clientHandlers: Handler[];
