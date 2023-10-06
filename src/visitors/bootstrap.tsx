d10ae1241aa9adb0b382145833483280510d52ac
Scott Pringle <pringlesc91@gmail.com>
2023-10-06 16:37:30 +0100

    Session ID is not the first param...

--- src/visitors/bootstrap.tsx
+++ src/visitors/bootstrap.tsx
@@ -76,7 +76,7 @@ const getSessionIdAndEndTime = (
     sessionId = uuidv4()
   } else {
     const c = cookieData as string
-    const [sessId] = c.split('|')
+    const [, sessId] = c.split('|')
     sessionId = sessId
   }
 
