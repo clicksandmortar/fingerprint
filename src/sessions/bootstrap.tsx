b0efd11cdc9c094d2dc932ee1fe85552deb99836
Scott Pringle <pringlesc91@gmail.com>
2023-10-06 16:05:17 +0100

    Do hacky stuff to not introduce a new cookie (#27)
    * Do hacky stuff to not introduce a new cookie
    
    * Remove unused dep

--- src/sessions/bootstrap.tsx
+++ src/sessions/bootstrap.tsx
@@ -1,7 +1,5 @@
 import { Session } from './types'
 import { getCookie, setCookie } from '../utils/cookies'
-import { v4 as uuidv4 } from 'uuid'
-
 
 export const bootstrapSession = ({
   appId,
@@ -11,22 +9,8 @@ export const bootstrapSession = ({
   setSession: (session: Session) => void
 }) => {
   const session: Session = {
-    firstVisit: undefined,
+    firstVisit: undefined
   }
-  const t = new Date()
-  t.setMinutes(t.getMinutes() + 30)
-
-  // Handle session cookie pices
-  if (!getCookie('_cm_session') || hasCookieValueExpired(getCookie('_cm_session'))) {
-    session.id = uuidv4()
-  } else {
-    const c = getCookie('_cm_session') as string;
-    const [sessionId] = c.split('|')
-    session.id = sessionId;
-  }
-
-  session.endTime = t
-  setCookie('_cm_session', `${session.id}|${session.endTime.toISOString()}`, undefined)
 
   if (!getCookie('_cm') || getCookie('_cm') !== appId) {
     setCookie('_cm', appId, 365)
@@ -41,16 +25,4 @@ export const bootstrapSession = ({
 
     setSession(session)
   }
-}
-const hasCookieValueExpired = (cookieData: string | undefined): Boolean => {
-  if (!cookieData) return true;
-  const [, timestampString] = cookieData.split('|');
-  const expiryTimeEpoch = Date.parse(timestampString);
-  const expiryTime = new Date()
-  expiryTime.setTime(expiryTimeEpoch)
-  const n = new Date()
-  if (n > expiryTime) {
-    return true
-  }
-  return false
 }
--- src/visitors/bootstrap.tsx
+++ src/visitors/bootstrap.tsx
@@ -2,11 +2,16 @@ import { v4 as uuidv4 } from 'uuid'
 import { Visitor } from './types'
 import { validVisitorId } from './utils'
 import { getCookie, setCookie } from '../utils/cookies'
+import { Session } from '../sessions/types'
 
 export const bootstrapVisitor = ({
-  setVisitor
+  setVisitor,
+  session,
+  setSession
 }: {
   setVisitor: (session: Visitor) => void
+  session: Session
+  setSession: (session: Session) => void
 }) => {
   const visitor: Visitor = {
     id: undefined
@@ -15,9 +20,18 @@ export const bootstrapVisitor = ({
   if (!getCookie('_cm_id') || !validVisitorId(getCookie('_cm_id') as string)) {
     const visitorId = uuidv4()
 
-    setCookie('_cm_id', visitorId, 365)
+    const { sessionId, endTime } = getSessionIdAndEndTime(getCookie('_cm_id'))
+
+    setCookie(
+      '_cm_id',
+      `${visitorId}|${sessionId}|${endTime.toISOString}.`,
+      365
+    )
 
     visitor.id = visitorId
+    session.id = sessionId
+    session.endTime = endTime
+    setSession(session)
 
     setVisitor(visitor)
 
@@ -25,8 +39,60 @@ export const bootstrapVisitor = ({
   }
 
   if (getCookie('_cm_id')) {
-    visitor.id = getCookie('_cm_id')
+    const c = getCookie('_cm_id') as string
+    const [visitorId] = c.split('|')
+    const { sessionId, endTime } = getSessionIdAndEndTime(getCookie('_cm_id'))
 
+    setCookie(
+      '_cm_id',
+      `${visitorId}|${sessionId}|${endTime.toISOString}.`,
+      365
+    )
+
+    visitor.id = visitorId
     setVisitor(visitor)
+
+    session.id = sessionId
+    session.endTime = endTime
+    setSession(session)
   }
+}
+
+const getSessionIdAndEndTime = (
+  cookieData: string | undefined
+): { sessionId: string; endTime: Date } => {
+  const t = new Date()
+  t.setMinutes(t.getMinutes() + 30)
+
+  let sessionId
+  const endTime = t
+  // Handle session cookie pices
+  if (!cookieData || hasCookieValueExpired(cookieData)) {
+    sessionId = uuidv4()
+  } else {
+    const c = cookieData as string
+    const [sessId] = c.split('|')
+    sessionId = sessId
+  }
+
+  return {
+    sessionId,
+    endTime
+  }
+}
+
+const hasCookieValueExpired = (cookieData: string | undefined): Boolean => {
+  if (!cookieData) return true
+  const cookieSplit = cookieData.split('|')
+  if (cookieSplit.length > 1) {
+    const timestampString = cookieSplit[cookieSplit.length - 1]
+    const expiryTimeEpoch = Date.parse(timestampString)
+    const expiryTime = new Date()
+    expiryTime.setTime(expiryTimeEpoch)
+    const n = new Date()
+    if (n > expiryTime) {
+      return true
+    }
+  }
+  return false
 }
