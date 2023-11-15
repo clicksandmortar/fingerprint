ad3d7680a988dfec90a35baba0f1c6640dc8814b
Scott Pringle <pringlesc91@gmail.com>
2023-11-15 09:12:08 +0000

    Log out form data first

--- src/context/CollectorContext.tsx
+++ src/context/CollectorContext.tsx
@@ -15,6 +15,7 @@ import { hasVisitorIDInURL } from '../utils/visitor_id'
 import { useLogging } from './LoggingContext'
 import { useMixpanel } from './MixpanelContext'
 import { useVisitor } from './VisitorContext'
+import useFormCollector from '../hooks/useFormCollector'
 
 const defaultIdleStatusDelay = 5 * 1000
 
@@ -255,6 +256,8 @@ export function CollectorProvider({
       handler: fireExitTrigger
     })
   }, [exitIntentTriggers, fireExitTrigger, log, registerHandler])
+
+  useFormCollector()
 
   const fireOnLoadTriggers = useCallback(() => {
     if (!pageLoadTriggers) return
---
+++ src/hooks/useFormCollector.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useVisitor } from "../context/VisitorContext";
+// import { useCollectorMutation } from "./useCollectorMutation";
+import { useLogging } from "../context/LoggingContext";
+
+export default function useFormCollector() {
+  // const { mutateAsync: collect } = useCollectorMutation()
+  const { visitor } = useVisitor();
+  const { log } = useLogging()
+
+  if (!visitor.id) return
+
+  useEffect(() => {
+    const forms = document.getElementsByTagName('form');
+    for (let i = 0; i < forms.length; i++) {
+      const f = forms[i];
+      f.addEventListener("submit", e => {
+        if (!e) return
+        const data = e?.currentTarget?.elements
+        log(data)
+      })
+    }
+  }, [])
+}
