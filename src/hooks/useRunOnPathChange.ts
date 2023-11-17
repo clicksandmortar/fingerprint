116cd150ef7b50b6454788c5be192a60c1cedc75
Scott Pringle <pringlesc91@gmail.com>
2023-11-17 11:12:14 +0000

    Switch to just use location.href;

--- src/hooks/useRunOnPathChange.ts
+++ src/hooks/useRunOnPathChange.ts
@@ -9,34 +9,26 @@ type Config = {
 // takes a function and runs it when the path changes
 // optionally takes a skip condition and init delay
 const useRunOnPathChange = (func: FuncProp, config?: Config) => {
-  const [lastCollectedPath, setLastCollectedPath] = useState<string>('')
-  const [lastCollectedHash, setLastCollectedHash] = useState<string>('')
-  const [lastCollectedQuery, setLastCollectedQuery] = useState<string>('')
+  const [lastCollected, setLastCollected] = useState<string>('')
   const { log } = useLogging()
 
   useEffect(() => {
     if (config?.skip) return
-    if (!location.pathname) return
-    if (
-      location.pathname === lastCollectedPath
-      && location.hash === lastCollectedHash
-      && location.search === lastCollectedQuery
-    ) return
+    if (!location.href) return
+    if (location.href === lastCollected) return
 
     // added timeout to prevent occasional double firing on page load
     const tId = setTimeout(() => {
-      log('useRunOnPathChange: running for path: ', location.pathname)
+      log('useRunOnPathChange: running for path: ', location.href)
 
-      setLastCollectedPath(location.pathname)
-      setLastCollectedHash(location.hash)
-      setLastCollectedQuery(location.search)
+      setLastCollected(location.href)
       func()
     }, config?.delay || 300)
 
     return () => {
       clearTimeout(tId)
     }
-  }, [location.pathname, location.hash, func, setLastCollectedPath, config])
+  }, [location.href, func, setLastCollected, config])
 }
 
 export default useRunOnPathChange
