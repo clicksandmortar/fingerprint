ecc273e510385fea9650eff35e1de069330f534d
dbacall <dbacall@hotmail.co.uk>
2023-11-22 15:41:03 +0000

    call useRunOnPathChange to fireOnLoadTriggers

--- src/context/CollectorContext.tsx
+++ src/context/CollectorContext.tsx
@@ -542,6 +542,11 @@ export function CollectorProvider({
     delay: initialDelay
   })
 
+  useRunOnPathChange(fireOnLoadTriggers, {
+    skip: !booted,
+    delay: initialDelay
+  })
+
   useFormCollector()
   useButtonCollector()
 
