ac7c958d8549481ef6d580b984d85580d794c823
Ed 'Catboy' Putans <edputans@gmail.com>
2023-11-22 12:17:31 +0200

    update script

--- src/widget/Widget.tsx
+++ src/widget/Widget.tsx
@@ -4,9 +4,8 @@ import { FingerprintProvider } from '../context/FingerprintContext'
 type Props = {
   appId?: string
   consent?: boolean
-  debug?: boolean
 }
 
-export const Widget = ({ appId, consent, debug }: Props) => {
-  return <FingerprintProvider appId={appId} consent={consent} debug={debug} />
+export const Widget = ({ appId, consent }: Props) => {
+  return <FingerprintProvider appId={appId} consent={consent} />
 }
