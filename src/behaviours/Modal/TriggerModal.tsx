cae05ecd5864abb04d3e919b536284b507a29d6e
Ed 'Catboy' Putans <edputans@gmail.com>
2023-12-19 14:35:26 +0200

    [Jan] DIFI-485: remove legacy code

--- src/behaviours/Modal/TriggerModal.tsx
+++ src/behaviours/Modal/TriggerModal.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { isMobile } from 'react-device-detect'
 import ReactDOM from 'react-dom'
 import { Trigger } from '../../client/types'
 import { useMixpanel } from '../../context/MixpanelContext'
 import { useCollectorMutation } from '../../hooks/api/useCollectorMutation'
 import { useSeenMutation } from '../../hooks/api/useSeenMutation'
-import { useBrand } from '../../hooks/useBrandConfig'
 import { useCollector } from '../../hooks/useCollector'
 import { HandleCloseOptions } from './Modal.types'
-import { BrownsModal } from './modals/browns'
-import CnMStandardModal from './modals/StandardModal'
-import StonehouseModal from './modals/stonehouse'
+import CnMModal from './modals/StandardModal'
 
 type Props = {
   trigger: Trigger
@@ -26,7 +22,6 @@ const Modal = ({ trigger }: Props) => {
 
   const { mutate: collect } = useCollectorMutation()
 
-  const brand = useBrand()
   const { mutate: runSeen, isSuccess, isLoading } = useSeenMutation()
 
   useEffect(() => {
@@ -86,67 +81,7 @@ const Modal = ({ trigger }: Props) => {
     handleCloseModal: handleCloseModal
   }
 
-  switch (brand) {
-    // NOTE: these are just temp for go-live, soon we will combine all of these into one modal
-    // with support for all underlying behaviours + potential scale config
-    case 'Ember': {
-      // /tablebooking > Christmas + BOOK NOW
-      let image = isMobile
-        ? 'https://cdn.fingerprint.host/assets/ember/emb-2023-intentlyscreen-christmas-booknow-m.jpg'
-        : 'https://cdn.fingerprint.host/assets/ember/emb-2023-intentlyscreen-christmas-booknow.jpg'
-
-      // /nationalsearch > Christmas + FIND OUT MORE
-      if (window.location.href.includes('nationalsearch'))
-        image = isMobile
-          ? `https://cdn.fingerprint.host/assets/ember/emb-2023-intentlyscreen-christmas-findoutmore-m.jpg`
-          : `https://cdn.fingerprint.host/assets/ember/emb-2023-intentlyscreen-christmas-findoutmore.jpg`
-
-      return (
-        <CnMStandardModal
-          {...modalProps}
-          trigger={{
-            ...trigger,
-            // override.... because branding.
-            data: {
-              ...trigger.data,
-              backgroundURL: image
-            }
-          }}
-        />
-      )
-    }
-    case 'Sizzling': {
-      // Find A Pub  > Christmas  + BOOK NOW
-      let image = isMobile
-        ? `https://cdn.fingerprint.host/assets/sizzling/siz-2023-intentlyscreen-christmas-booknow-m.jpg`
-        : `https://cdn.fingerprint.host/assets/sizzling/siz-2023-intentlyscreen-christmas-booknow.jpg`
-
-      // Sign Up  > Christmas  + FIND OUT MORE
-      if (window.location.href.includes('signup'))
-        image = isMobile
-          ? `https://cdn.fingerprint.host/assets/sizzling/siz-2023-intentlyscreen-christmas-findoutmore-m.jpg`
-          : `https://cdn.fingerprint.host/assets/sizzling/siz-2023-intentlyscreen-christmas-findoutmore.jpg`
-
-      return (
-        <CnMStandardModal
-          {...modalProps}
-          trigger={{
-            ...trigger,
-            // override.... because branding.
-            data: { ...trigger.data, backgroundURL: image }
-          }}
-        />
-      )
-    }
-    // we can soon phase out these 2 brand specific ones
-    case 'Stonehouse':
-      return <StonehouseModal {...modalProps} />
-    case 'Browns':
-      return <BrownsModal {...modalProps} />
-    case 'C&M':
-    default:
-      return <CnMStandardModal {...modalProps} />
-  }
+  return <CnMModal {...modalProps} />
 }
 
 export const TriggerModal = ({ trigger }: Props) => {
